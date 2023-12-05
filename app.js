9f16f864ddc322a1f82ecac28944689301a89488
Kamil <111041043+B1zy@users.noreply.github.com>
2023-12-05 21:15:26 +0100

    ALLE dateien von meinen PC die den LB gehörten hochgeladen

--- app.js
+++ app.js
@@ -1,208 +1,235 @@
-// Beispiel-JSON-Objekt
-const jsonData = {
-    "entries": [
-    
-        {"title" :"Eintrag eins",
-        "id":"1",
-        "description":"Mein erstes Eintrag",
-        "author":"Autor:Kamil",
-        "content":"Hey Tagebuch heute war 'n Tag. Morgenkaffee, Arbeit, bisschen Erfolg. Aber irgendwie fehlt was. Vielleicht mehr Action, weniger Langeweile? Ich denk drüber nach. Triffst Leute, sie haben ihre eigenen Deals. Das macht mich nachdenklich. Was will ich wirklich? Mehr Kick, mehr Spaß. Mal sehen, wie ich das hinbiegen kann. Schreib später mehr.",
-        "creationDate":"Erstelldatum:04.12.2023",
-        "creationHour":"12:00",
-        "appendix":"false"
-        },
-        {"title" :"Eintrag zwei",
-        "id":"2",
-        "description":"Mein zweites Eintrag",
-        "author":"Autor:Kamil",
-        "content":"Moin Tagebuch, heute geht's um 'nen speziellen Kram. Einfach drauflos schreiben, ohne Filter. Mal gucken, was dabei rauskommt. Vielleicht 'ne Menge, vielleicht nicht viel. Aber hey, das ist das Spiel. bis dann, kamil",
-        "creationDate":"Erstelldatum:05.12.2023",
-        "creationHour":"13:00",
-        "appendix":"false"
-        },
-        {"title" :"Eintrag drei",
-        "id":"3", 
-        "description":"Mein drittes Eintrag", 
-        "author":"Autor:Kamil",
-        "content": "Moin Tagebuch, heute brech' ich mal 'nen Unterschied runter. Kein Experten-Gelaber, nur meine Sicht. Keine Ahnung, ob's jemanden interessiert, aber es tut gut, es aufzuschreiben. Vielleicht klärt sich was in meinem Kopf, vielleicht nicht. Bis dann, kamil",
-        "creationDate":"Erstelldatum:06.12.2023",
-        "creationHour":"14:00",
-        "appendix":"false"
-        }
-        
-    ]
-  };
-  
-// Fetch entries
-fetch('http://localhost:3000/entries')
-  .then(response => response.json())
-  .then(entries => {
-    // Handle the retrieved entries
-    console.log(entries);
-  })
-  .catch(error => console.error('Error fetching entries:', error));
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('WebAPI.json')
+      .then(response => response.json())
+      .then(data => {
+          const diaryContainer = document.getElementById('diary-container');
+          data.entries.forEach(entry => {
+              const entryDiv = document.createElement('div');
+              const entryTitle = document.createElement('span');
+              const viewButton = document.createElement('button');
 
-  
-  
-  // Funktion zum Anzeigen der Einträge
-  function displayEntries(entries) {
-    const mainContent = document.getElementById("mainContent");
-    mainContent.innerHTML = ''; // leeren Sie den Hauptbereich, um Aktualisierungen vorzunehmen
-  
-    entries.forEach(entry => {
-        const entryDiv = document.createElement("div");
-        entryDiv.innerHTML = `<h2>${entry.title}</h2><p>${entry.description}</p><p>${entry.author}</p><p>${entry.creationDate}</p>`;
+              entryTitle.textContent = entry.title;
+              viewButton.textContent = 'View Details';
+              viewButton.onclick = () => displayDetails(entry);
+
+              entryDiv.appendChild(entryTitle);
+              entryDiv.appendChild(viewButton);
+              diaryContainer.appendChild(entryDiv);
+              
+          });
+      })
       
-        // Detailansicht anzeigen Button hinzufügen
-        const detailButton = document.createElement("button");
-        detailButton.textContent = "Details anzeigen";
-        detailButton.addEventListener("click", () => showDetailView(entry.id));
+      .catch(error => console.error('Error loading diary entries:', error));
+});
 
-        // Bearbeiten- und Löschen-Buttons hinzufügen
-        const editButton = document.createElement("button");
-        editButton.textContent = "Bearbeiten";
-        editButton.addEventListener("click", () => editEntry(entry.id));
-  
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Löschen";
-        deleteButton.addEventListener("click", () => deleteEntry(entry.id));
-
-        entryDiv.appendChild(detailButton);
-        entryDiv.appendChild(editButton);
-        entryDiv.appendChild(deleteButton);
-  
-        mainContent.appendChild(entryDiv);
-    });
-}
-
-  function showDetailView(entryId) {
-    const entry = jsonData.entries.find(e => e.id === entryId);
-
-    // Setzen Sie die Detailansicht mit den Daten des Eintrags
-    document.getElementById("detailTitle").textContent = entry.title;
-    document.getElementById("detailDescription").textContent = entry.description;
-    document.getElementById("detailAuthor").textContent = entry.author;
-    document.getElementById("detailContent").textContent = entry.content;
-    document.getElementById("detailCreationDate").textContent = entry.creationDate;
-    document.getElementById("detailCreationHour").textContent = entry.creationHour;
-
-    // Formular und Hauptinhalt ausblenden, Detailansicht einblenden
-    document.getElementById("mainContent").style.display = "none";
-    document.getElementById("editForm").style.display = "none";
-    document.getElementById("detailView").style.display = "block";
-}
-
-
-// Funktion zum Zurückkehren zur Hauptansicht
-function backToMain() {
-  document.getElementById("mainContent").style.display = "block";
-  document.getElementById("detailView").style.display = "none";
-  // Fügen Sie hier ggf. zusätzlichen Code hinzu, um die Detailansicht zurückzusetzen
-}
-
-  // Funktion zum Hinzufügen eines neuen Eintrags
-  function addEntry(newEntry) {
-    jsonData.entries.push(newEntry);
-    displayEntries(jsonData.entries);
+function displayDetails(entry) {
+  const detailsContainer = document.getElementById('details-container');
+  if (!detailsContainer) {
+      console.error('Details container not found in the HTML');
+      return;
   }
-  
- // Funktion zum Bearbeiten eines Eintrags
- function editEntry(entryId) {
-  const entryToEdit = jsonData.entries.find(entry => entry.id === entryId);
 
-  // Das Formular mit den Daten des zu bearbeitenden Eintrags vorfüllen
-  document.getElementById("editTitle").value = entryToEdit.title;
-  document.getElementById("editDescription").value = entryToEdit.description;
-  document.getElementById("editContent").value = entryToEdit.content;
-  document.getElementById("editAuthor").value = entryToEdit.author;
-  document.getElementById("editCreationDate").value = entryToEdit.creationDate;
-  document.getElementById("editCreationHour").value = entryToEdit.creationHour;
-  document.getElementById("editAppendix").value = entryToEdit.appendix;
+  // Clear existing content
+  detailsContainer.innerHTML = '';
 
-  // Setzen Sie die editId für den späteren Zugriff
-  document.getElementById("editId").value = entryId;
+  // Create and append new content for the selected entry
+  const titleElement = document.createElement('h3');
+  titleElement.textContent = entry.title;
 
-  // Formular einblenden und Hauptinhalt ausblenden
-  document.getElementById("mainContent").style.display = "none";
-  document.getElementById("detailView").style.display = "none"; // Verbergen Sie die Detailansicht
-  document.getElementById("editForm").style.display = "block";
+  const descriptionElement = document.createElement('p');
+  descriptionElement.textContent = entry.description;
+
+  const authorElement = document.createElement('p');
+  authorElement.textContent = 'Author: ' + entry.author;
+
+  const contentElement = document.createElement('p');
+  contentElement.textContent = entry.content;
+
+  const creationDateElement = document.createElement('p');
+  creationDateElement.textContent = 'Created on: ' + entry.creationDate + ' at ' + entry.creationHour;
+
+  detailsContainer.appendChild(titleElement);
+  detailsContainer.appendChild(descriptionElement);
+  detailsContainer.appendChild(authorElement);
+  detailsContainer.appendChild(contentElement);
+  detailsContainer.appendChild(creationDateElement);
 }
-// Funktion zum Speichern der Bearbeitung
-function saveEdit() {
-  // Hier können Sie die Logik zum Speichern der Bearbeitung implementieren
-  const editedEntry = {
-      "title": document.getElementById("editTitle").value,    
-      "description": document.getElementById("editDescription").value,
-      "content": document.getElementById("editContent").value,
-      "author": document.getElementById("editAuthor").value,
-      "creationDate": document.getElementById("editCreationDate").value,
-      "creationHour": document.getElementById("editCreationHour").value,
-      "appendix": document.getElementById("editAppendix").value
-  };
+document.addEventListener('DOMContentLoaded', function() {
+  // Existing code to load diary entries...
 
-  // Suchen Sie den bearbeiteten Eintrag im JSON-Objekt
-  const entryId = document.getElementById("editId").value;
-  const indexToEdit = jsonData.entries.findIndex(entry => entry.id === entryId);
+  const newEntryButton = document.getElementById('new-entry-button');
+  newEntryButton.onclick = () => showNewEntryForm();
+});
 
-  // Überprüfen Sie, ob der Eintrag gefunden wurde
-  if (indexToEdit !== -1) {
-      // Aktualisieren Sie den Eintrag mit den bearbeiteten Werten
-      jsonData.entries[indexToEdit] = { ...jsonData.entries[indexToEdit], ...editedEntry };
-      
-      // Aktualisierte Daten in localStorage speichern
-      localStorage.setItem('entries', JSON.stringify(jsonData.entries));
-      
-      // Anzeige aktualisieren
-      displayEntries(jsonData.entries);
+function showNewEntryForm() {
+  const formContainer = document.getElementById('new-entry-form');
+  if (!formContainer) {
+      console.error('New entry form container not found in the HTML');
+      return;
   }
-  
-  // Zurück zur Hauptansicht navigieren
-  window.location.href = 'index.html';
+
+  // Clear existing content and display the form
+  formContainer.innerHTML = '';
+  formContainer.style.display = 'block';
+
+  const form = document.createElement('form');
+  form.id = 'entry-form';
+  form.innerHTML = `
+  <input type="text" id="new-title" placeholder="Title">
+  <input type="text" id="new-author" placeholder="Author">
+  <textarea id="new-content" placeholder="Content"></textarea>
+  <button type="button" onclick="addNewEntry()">Submit</button>
+  `;
+  formContainer.appendChild(form);
 }
 
-    // Funktion zum Laden der Daten beim Start der Anwendung
-    function loadJsonData() {
-        const storedEntries = localStorage.getItem('entries');
-        if (storedEntries) {
-            return JSON.parse(storedEntries);
-        }
-        return jsonData.entries;
-    }
-    // Formular ausblenden und Hauptinhalt einblenden
-    document.getElementById("mainContent").style.display = "block";
-    document.getElementById("editForm").style.display = "none";
-
-    // Nach dem Speichern die aktualisierten Einträge anzeigen
-    displayEntries(jsonData.entries);
+function addNewEntry() {
+  const title = document.getElementById('new-title').value;
+  const author = document.getElementById('new-author').value;
+  const content = document.getElementById('new-content').value;
+  const creationDate = new Date().toLocaleDateString(); // Format: 'MM/DD/YYYY'
+  const creationTime = new Date().toLocaleTimeString(); // Format: 'HH:MM:SS AM/PM'
 
 
-// Funktion zum Abbrechen der Bearbeitung
-function cancelEdit() {
-    // Formular ausblenden und Hauptinhalt einblenden
-    document.getElementById("mainContent").style.display = "block";
-    document.getElementById("editForm").style.display = "none";
-}  
-  // Funktion zum Löschen eines Eintrags
-  function deleteEntry(entryId) {
-    jsonData.entries = jsonData.entries.filter(entry => entry.id !== entryId);
-    displayEntries(jsonData.entries);
-  }
-  
-  // Beispielaufruf der Funktionen
-  const entriesData = loadJsonData();
-  displayEntries(entriesData);
-  
-  // Beispiel: Hinzufügen eines neuen Eintrags
   const newEntry = {
-    "title": "Neuer Eintrag",
-    "id": "4",
-    "description": "Ein neuer Eintrag für den Tagebuch",
-    "author": "Autor: ",
-    "content": "Hier steht der Inhalt des Eintrags",
-    "creationDate": "Erstelldatum: ",
-    "creationHour": "Erstellungszeit: ",
-    "appendix": "false"
+    title,
+    author,
+    content,
+    creationDate,
+    creationHour: creationTime,
+    // Add any other necessary fields here
+};
+
+// Send the new entry to the backend
+fetch('http://localhost:2940/api/v1/entities', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newEntry)
+})
+.then(response => {
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw new Error('Failed to create new entry');
+    }
+})
+.then(data => {
+    console.log('New entry created:', data);
+    // Optionally, update the UI to include the new entry
+    appendNewEntryToUI(data);
+})
+.catch(error => console.error('Error creating new entry:', error));
+
+// Hide the form after submission
+document.getElementById('new-entry-form').style.display = 'none';
+
+}
+function appendNewEntryToUI(entry) {
+  const diaryContainer = document.getElementById('diary-container');
+  const entryDiv = document.createElement('div');
+  const entryTitle = document.createElement('span');
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.onclick = () => editEntry(entry, entryDiv);
+
+  entryTitle.textContent = entry.title;
+
+  const viewButton = document.createElement('button');
+  viewButton.textContent = 'View Details';
+  viewButton.onclick = () => displayDetails(entry);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.onclick = () => deleteEntry(entry, entryDiv);
+
+  entryDiv.appendChild(entryTitle);
+  entryDiv.appendChild(viewButton);
+  entryDiv.appendChild(deleteButton); // Add the delete button
+  entryDiv.appendChild(editButton);
+  diaryContainer.appendChild(entryDiv);
+}
+function deleteEntry(entry, entryDiv) {
+  // Here you would send a request to your backend to delete the entry
+   fetch('http://localhost:2940/api/v1/entities', { method: 'DELETE', body: JSON.stringify({ id: entry.id }) });
+
+  // Remove the entry from the UI
+  entryDiv.remove();
+}
+function editEntry(entry, entryDiv) {
+  entryDiv.innerHTML = '';
+  
+  const titleInput = document.createElement('input');
+  titleInput.type = 'text';
+  titleInput.value = entry.title;
+  titleInput.id = `edit-title-${entry.id}`;
+
+  const authorInput = document.createElement('input');
+  authorInput.type = 'text';
+  authorInput.value = entry.author;
+  authorInput.id = `edit-author-${entry.id}`;
+
+  const contentTextarea = document.createElement('textarea');
+  contentTextarea.value = entry.content;
+  contentTextarea.id = `edit-content-${entry.id}`;
+
+  const saveButton = document.createElement('button');
+  saveButton.textContent = 'Save';
+  saveButton.onclick = () => submitEdit(entry.id, entryDiv, titleInput, authorInput, contentTextarea);
+
+  const cancelButton = document.createElement('button');
+  cancelButton.textContent = 'Cancel';
+  cancelButton.onclick = () => cancelEdit(entry.id, entryDiv, entry);
+
+  entryDiv.appendChild(titleInput);
+  entryDiv.appendChild(authorInput);
+  entryDiv.appendChild(contentTextarea);
+  entryDiv.appendChild(saveButton);
+  entryDiv.appendChild(cancelButton);
+}
+
+function submitEdit(entryId, entryDiv, titleInput, authorInput, contentTextarea) {
+  const editedTitle = titleInput.value;
+  const editedAuthor = authorInput.value;
+  const editedContent = contentTextarea.value;
+
+  const updatedEntry = {
+      id: entryId,
+      title: editedTitle,
+      author: editedAuthor,
+      content: editedContent,
+      // ...other fields if necessary...
   };
-  
-  addEntry(newEntry);
-  
+
+  // ...send request to backend and handle response...
+
+  updateEntryDisplay(entryDiv, updatedEntry);
+}
+function cancelEdit(entryId, entryDiv, originalEntry) {
+  // Restore the original display of the entry
+  updateEntryDisplay(entryDiv, originalEntry);
+}
+function updateEntryDisplay(entryDiv, entry) {
+  entryDiv.innerHTML = '';
+
+  const entryTitle = document.createElement('span');
+  entryTitle.textContent = entry.title;
+
+  const viewButton = document.createElement('button');
+  viewButton.textContent = 'View Details';
+  viewButton.onclick = () => displayDetails(entry);
+
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.onclick = () => editEntry(entry, entryDiv);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.onclick = () => deleteEntry(entry, entryDiv);
+
+  entryDiv.appendChild(entryTitle);
+  entryDiv.appendChild(viewButton);
+  entryDiv.appendChild(editButton);
+  entryDiv.appendChild(deleteButton);
+}
